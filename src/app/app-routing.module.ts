f20efb9a6fa0003f3147a8acf73bc1efec7ba3b1
dingruiqi <dingruiqi1981@163.com>
2019-10-27 20:31:41 +0800

    123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErpHomeComponent } from './layout-framework/erp-home/erp-home.component';
 import { AuthGuard } from './module/auth/auth.guard';
-import { LoginComponent } from './module/auth/login/login.component';
 
 
 const routes: Routes = [{
